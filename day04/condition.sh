1e64bc3760897d7b92f977d44e50cbdff6fa3ddf
rajbirathwal6 <useitforyou06@gmail.com>
2024-04-03 19:19:27 +0000

    Bashing script with conditional statements

---
+++ day04/condition.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$1" -gt  "$2" ];
+then 
+	echo "$1 is greater then $2"
+else
+	echo "$2 is greater"
+fi
---
+++ day04/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "I will complete #90DaysOfDevops challenge"
+
+read input1
+
+echo "Here is the variable"
+echo $input1
+
+echo "Here is the argument"
+echo "$1"
+
+
